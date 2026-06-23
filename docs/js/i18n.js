0791b373f65db92fc5b5079c2006b30ee957ecfd
nigeldearden-a11y <259474807+nigeldearden-a11y@users.noreply.github.com>
2026-06-23 14:00:47 -0400

    Force EN default: bump i18n migration to v3, add Deck 3 + Golden Excel links to turing-paper.html

--- docs/js/i18n.js
+++ docs/js/i18n.js
@@ -40,7 +40,7 @@
      */
     function init() {
         // v2 migration: reset all users to English (June 2026 fix)
-        const migrationKey = 'ia-lang-v2';
+        const migrationKey = 'ia-lang-v3';
         if (!localStorage.getItem(migrationKey)) {
             localStorage.setItem(STORAGE_KEY, 'en');
             localStorage.setItem(migrationKey, '1');
